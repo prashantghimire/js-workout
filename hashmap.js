4f625af08d94f89ece47a21378cc1417d561dfec
Prashant Ghimire <prashant.ghimire@leviton.com>
2019-01-28 16:19:10 -0600

    added hashmap progress

---
+++ hashmap.js
@@ -0,0 +1,10 @@
+function Hashmap (size){
+    this.data = new Array(size).fill(null);
+}
+
+// create array of the size, recreate double on index out of bound
+// calculate index for each value and put item in that index 
+Hashmap.prototype.put = function (key, value){
+};
+
+let data = ['alice', 'bob', 'carol', 'david'];
