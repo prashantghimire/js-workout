39214c0722acdcbb2f265f50cec1d8819948c1b0
Prashant Ghimire <prasmire@gmail.com>
2019-01-28 23:07:04 -0600

    added contains method in tree

--- tree.js
+++ tree.js
@@ -20,6 +20,25 @@ Node.prototype.insert = function (data){
     }
 };
 
+Node.prototype.contains = function (data) {
+
+    if(data == this.data){
+        return true;
+    } else if (data < this.data) {
+        if(this.left != null){
+            return this.left.contains(data);
+        } else {
+            return data === this.data;
+        }
+    } else {
+        if(this.right != null){
+            return this.right.contains(data);
+        } else {
+            return data === this.data;
+        }
+    }
+};
+
 Node.prototype.printInOrder = function (){
     if(this.left){
         this.left.printInOrder();
@@ -55,6 +74,14 @@ root.insert(3);
 root.insert(6);
 root.insert(4);
 root.insert(2);
+root.insert(8);
 root.insert(7);
 
 root.postOrder();
+
+
+for (let i = 0; i < (new Array(10)).length; i++){
+    const num = i;
+    const contains5 = root.contains(num);
+    console.log('contains '+num+' : ' + contains5);
+}
