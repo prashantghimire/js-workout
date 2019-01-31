5deaa57e6f84b8b0a1a31482e945001231fadc9a
Prashant Ghimire <prasmire@gmail.com>
2019-01-30 19:25:16 -0600

    added equality check method in tree

--- tree.js
+++ tree.js
@@ -39,6 +39,23 @@ Node.prototype.contains = function (data) {
     }
 };
 
+Node.equals = function (node1, node2){
+    
+    if (node1 == node2) return true;
+    if (node1 == null || node2 == null) return false;
+
+    if (node1.data != node2.data){
+        return false;
+    }
+    if(!Node.equals(node1.left, node2.left)){
+        return false;
+    }
+    if (!Node.equals(node1.right, node2.right)){
+        return false;
+    }
+    return true;
+};
+
 Node.prototype.printInOrder = function (){
     if(this.left){
         this.left.printInOrder();
@@ -77,6 +94,7 @@ root.insert(2);
 root.insert(8);
 root.insert(7);
 
+/*
 root.postOrder();
 
 
@@ -85,3 +103,23 @@ for (let i = 0; i < (new Array(10)).length; i++){
     const contains5 = root.contains(num);
     console.log('contains '+num+' : ' + contains5);
 }
+*/
+
+let groot = new Node(5);
+groot.insert(3);
+groot.insert(6);
+groot.insert(4);
+groot.insert(2);
+groot.insert(8);
+groot.insert(7);
+
+root.postOrder();
+console.log(`--- breaker ---`);
+groot.postOrder();
+
+function areEquals (node1, node2){
+    return Node.equals(node1, node2);
+}
+
+const areEq = areEquals(root, groot);
+console.log(`root and groot are equal : ${areEq}`);
