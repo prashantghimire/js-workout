2e66f14bf3451377cd0f68c78896c08d74bd40f9
Prashant Ghimire <prasmire@gmail.com>
2019-01-30 23:33:40 -0600

    added memoization for message encoding solution

--- encodemessage.js
+++ encodemessage.js
@@ -1,5 +1,5 @@
 let {performance} = require('perf_hooks');
-function combinator(string, count){
+function combinator(string, count, memo){
     
     if (count === 0){
         return 1;
@@ -10,19 +10,24 @@ function combinator(string, count){
         return 0;
     }
 
-    result = combinator(string, count - 1);
-    let firstTwo = parseInt(string.substr(0, 2));
-    if (count >= 2 && firstTwo <= 26){
-        result += combinator(string, count - 2);
+    if (memo[count]) {
+        return memo[count];
     }
 
-    return result;
+    result = combinator(string, count - 1, memo);
+    let firstTwo = parseInt(string.substr(0, 2));
+    if (count >= 2 && firstTwo <= 26){
+        result += combinator(string, count - 2, memo);
+    }
 
+    memo[count] = result;
+    return result;
 }
 
 let t1 = performance.now();
-let string = '1234567893746948329482937429837492374982374982374982734892';
-let response = combinator(string, string.length);
+let string = '12345678937469483294829374937469483294829374';
+let memo = new Array(1 + string.length).fill(null);
+let response = combinator(string, string.length, memo);
 let t2 = performance.now();
 console.log(`time taken : ${(t2 - t1).toFixed(5)} ms.`);
 console.log(response);
