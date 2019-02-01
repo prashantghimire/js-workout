ff78b285f3837e69d1e2441700f2a9ba2d842407
Prashant Ghimire <prashant.ghimire@leviton.com>
2019-02-01 14:09:13 -0600

    added another func with O(n)

--- array-multiple.js
+++ array-multiple.js
@@ -1,5 +1,6 @@
+const {performance} = require('perf_hooks');
+
 function printer (array){
-    console.log('input array is : ', array);
     let multiplication = [];
     for (let i = 0; i < array.length; i++){
         multiplication[i] = (function (){
@@ -11,9 +12,37 @@ function printer (array){
             }
             return currentMultiplication;
         }());
-    }    
+    }
     return multiplication;
 }
 
-let result = printer([1,2,3]);
-console.log('result array is : ' + result);
+function simplePrinter(array) {
+    
+    let total = 1;
+    let totalMultiplication = array.reduce((total, current)=> {
+        total *= current;
+        return total;
+    }, 1);
+
+    let result = [];
+    for (let i = 0; i < array.length; i++){
+        result[i] = totalMultiplication / array[i];
+    }
+    return result;
+}
+
+let arr = (new Array(1000000)).fill(0).map((item, index)=> (index + 1));
+let t1 = performance.now();
+let printerResult = printer(arr);
+let t2 = performance.now();
+
+console.log(`O(n^2) computation took : ${(t2 - t1).toFixed(3)} ms`);
+
+/*
+// console.log('result array is : ', printerResult);
+let t3 = performance.now();
+let simpleResult = simplePrinter(arr);
+let t4 = performance.now();
+console.log(`O(n) computation took : ${(t4 - t3).toFixed(3)} ms`);
+*/
+// console.log('result array is : ', simpleResult);
